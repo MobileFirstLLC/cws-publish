b28ecd257c93d5e7ee3c6eca3a25051c80a19cbc
Neea Rusch <neea.rusch@mobilefirst.me>
2021-08-09 17:46:43 -0400

    update jsdocs

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,43 @@
+/**
+ * Represents some Chrome WebStore API response.
+ */
 export interface APIResult {
     body: object
 }
 
+/**
+ * WebStore "Items:Update" response type.
+ *
+ * For endpoint specs:
+ * @see {@link https://developer.chrome.com/docs/webstore/webstore_api/items/update/}
+ *
+ * For response data:
+ * @see {@link https://developer.chrome.com/docs/webstore/webstore_api/items/}
+ */
 export class UploadResult implements APIResult {
     body: {
         uploadState: string
     }
 }
 
+/**
+ * WebStore "Items:Publish" response type.
+ *
+ * For endpoint specs:
+ * @see {@link https://developer.chrome.com/docs/webstore/webstore_api/items/publish/}
+ *
+ * For response data:
+ * @see {@link https://developer.chrome.com/docs/webstore/webstore_api/items/publish/#json-1}
+ */
 export class PublishResult implements APIResult {
     body: {
         status: Array<string>
     }
 }
 
+/**
+ * Internally wrap the API response into a success flag, and the raw response.
+ */
 export interface RequestResult {
     success: boolean,
     result: APIResult
